e667eb503f19bdb1e337552e0ec249355f7a2e70
Diego Alejandro Hidalgo Machaca <135664425+DiegoH81@users.noreply.github.com>
2024-04-28 08:41:32 -0500

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,15 @@
+class User():
+    id = 0
+    nombre = ''
+    user = ''
+    password = ''
+    email = ''
+    def __init__ (self, id, nombre, user, password, email):
+        self.id = id
+        self.nombre = nombre
+        self.user = user
+        self.password = password
+        self.email = email
+    
+    def check_password(self, in_password):
+        return self.password == in_password
